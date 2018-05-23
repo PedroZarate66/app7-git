74ec465fcd967b23d966ee48586ba82e29e2a543
Gustavo <alfaprime117@gmail.com>
2018-05-23 12:19:53 -0500

    subida23

--- js/acciones.js
+++ js/acciones.js
@@ -4,9 +4,12 @@ $(document).ready(function(e) {
     document.addEventListener("diviceready",function(){
 		$('#izquierda').on("swipeleft",function(){
 			navigator.notification.alert("deslizó hacia la izquierda",function(){"aplicación 7","OK"});
+    document.addEventListener("deviceready",function(){
+		$('#izquierda').on("swipeleft", function(){
+			navigator.notification.alert("deslizó hacia la izquierda", function(){"aplicacion 7","Ok"});
 		});//cerrar deslizar izquierda
 		$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm("¿Qué quiere hacer?",function(op){
+			navigator.notification.confirm("¿Qué quieres hacer?", function(op){
 				switch(op)
 				{
 					case 1:
@@ -17,7 +20,7 @@ $(document).ready(function(e) {
 					navigator.notification.vibrate(3000);
 					break;
 				}
-			},"aplicación 7","Beepear,Vibrar,Cancelar");
+			},"aplicacion 7","Beepear,Vibrar,Cancelar");
 		});//cerrar deslizar derecha
 	},false);//cerrar deviceready
 });//cerrar document
